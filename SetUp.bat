9f146d113251f8bd26c3bfa6ba0014b12b264143
Lopy <LorianTir@gmail.com>
2024-10-05 18:59:28 +0200

    Update SetUp.bat

--- SetUp.bat
+++ SetUp.bat
@@ -1,7 +1,9 @@
 @echo off
+echo Quel est le chemin d'acces de l'arborescense ?
+set /p arbre=
 echo Entrez votre webhook de log:
 set /p webhooklog=
 (
 echo.
 echo %webhooklog%
-) > E:\Programme\RemoteControl\Ressource\ProgramRessource\Ressource.txt
+) > %arbre%\Ressource\ProgramRessource\Ressource.txt
