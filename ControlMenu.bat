54dc5c0bc608fc5124e4242f63fb6462ee6dee80
Lopy <LorianTir@gmail.com>
2024-10-05 17:54:24 +0200

    Update ControlMenu.bat

--- ControlMenu.bat
+++ ControlMenu.bat
@@ -4,7 +4,8 @@ echo Entrez votre commande
 echo 1) RemoteShutdown
 echo 2) CloseApp
 echo 3) RemoteRestart
-set /p %command%=
+echo.
+set /p command=
 if %command%==RemoteShutdown goto Shutdown
 if %command%==1 goto Shutdown
 if %command%==CloseApp goto CloseApp
@@ -12,5 +13,9 @@ if %command%==2 goto CloseApp
 if %command%==RemoteRestart goto Restart
 if %command%==3 goto Restart
 :Shutdown
+echo.
+echo Entrez un webook de log:
+set /p webhook=
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Shutdown\"}" %webhook%
 :CloseApp
 :Restart
