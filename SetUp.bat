e3143556b426eeb6f0e15decc1331eaa1352536a
Lopy <LorianTir@gmail.com>
2024-10-06 10:36:37 +0200

    Delete SetUp.bat

--- SetUp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Quel est le chemin d'acces de l'arborescense ?
-set /p arbre=
-echo Entrez votre webhook de log:
-set /p webhooklog=
-(
-echo.
-echo %webhooklog%
-) > %arbre%\Ressource\ProgramRessource\Ressource.txt
