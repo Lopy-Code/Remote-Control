1d2af903cbabf5e56fe7c46192ffbb460367c096
Loupiaup207 <156239541+Loupiaup207@users.noreply.github.com>
2024-10-09 12:44:44 +0200

    Update ControlMenu.bat 0
    Ajoute d'#

--- ControlComputer/ControlMenu.bat
+++ ControlComputer/ControlMenu.bat
@@ -1,24 +1,43 @@
 @echo off
 color 02
+
+# Lecture de la ressource du webhook depuis un fichier texte
 for /F "skip=1 delims=" %%i in ('Type "E:\Programme\RemoteControl\Ressource\ProgramRessource\Ressource.txt"') do set "webhook=%%i"
+
+# Affichage des options de commande
 echo Entrez votre commande
 echo 1) RemoteShutdown
 echo 2) CloseApp
 echo 3) RemoteRestart
 echo.
+
+# Lecture de la commande saisie par l'utilisateur
 set /p command=
+
+# Vérification de la commande saisie et redirection vers les sections appropriées
 if %command%==RemoteShutdown goto Shutdown
 if %command%==1 goto Shutdown
 if %command%==CloseApp goto CloseApp
 if %command%==2 goto CloseApp
 if %command%==RemoteRestart goto Restart
 if %command%==3 goto Restart
+
 :Shutdown
 echo.
+# Envoi d'une requête POST pour éteindre le système
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Shutdown\"}" %webhook%
+goto end
+
 :CloseApp
 echo.
+# Envoi d'une requête POST pour fermer l'application spécifiée
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Close %ClosedApp%\"}" %webhook%
+goto end
+
 :Restart
+# Envoi d'une requête POST pour redémarrer le système
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Restart\"}" %webhook%
+goto end
+
+:end
 echo.
